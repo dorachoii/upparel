5aa3330839d54de30a3eccd16f50cfcbf16bd984
Dora Choi <135581648+dorachoii@users.noreply.github.com>
2024-09-18 16:26:32 +0900

    feat/ 애니메이션 동기화완료 대박
    feat/ 애니메이션 동기화완료 대박

--- Upparel_URP/Assets/_Dorami/01.Scripts/PlayerState.cs
+++ Upparel_URP/Assets/_Dorami/01.Scripts/PlayerState.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class PlayerState : MonoBehaviour
+public class PlayerState : MonoBehaviourPunCallbacks
 {
     public State currState;
     public Animator animator;
+    public PhotonView PV;
 
     public enum State
     {
@@ -23,16 +26,44 @@ public class PlayerState : MonoBehaviour
         {
             case State.IDLE:
                 animator.SetBool("isMoving", false);
+                PV.RPC("idleRpc", RpcTarget.All);
                 break;
             case State.WALK:
                 animator.SetBool("isMoving", true);
+                PV.RPC("walkRpc", RpcTarget.All);
                 break;
             case State.JUMP:
                 animator.SetTrigger("JumpTrigger");
+                PV.RPC("jumpRpc", RpcTarget.All);
                 break;
             case State.DANCE:
                 animator.SetTrigger("DanceTrigger");
+                PV.RPC("danceRpc", RpcTarget.All);
                 break;
         }
+    }
+
+    [PunRPC]
+    void idleRpc()
+    {
+       animator.SetBool("isMoving", false);
+    }
+
+    [PunRPC]
+    void walkRpc()
+    {
+       animator.SetBool("isMoving", true);
+    }
+
+    [PunRPC]
+    void jumpRpc()
+    {
+       animator.SetTrigger("JumpTrigger");
+    }
+
+    [PunRPC]
+    void danceRpc()
+    {
+       animator.SetTrigger("DanceTrigger");
     }
 }
