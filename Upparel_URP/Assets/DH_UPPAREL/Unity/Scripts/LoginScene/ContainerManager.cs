f4ba916f3c309655d39f9bfdfa2c026b574e0a92
Dora Choi <hellodoradora7@gmail.com>
2025-05-07 09:53:20 +0900

    renew

--- Upparel_URP/Assets/DH_UPPAREL/Unity/Scripts/LoginScene/ContainerManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ContainerManager : MonoBehaviour
-{
-    //컨테이너를 생성한다.
-
-}
--- Upparel_URP/Assets/_forCollectCloth/Scripts/TextHolder.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TextHolder : MonoBehaviour
-{
-    public string dayString = "";
-    public string timeString = "";
-    public string locationString = "";
-    public bool isMorning = true; // Default to true (morning)
-}
